5b7c1c8ca7749eadafb39083c767790913a2e980
Lenovo <petarstanojkovic@yahoo.ca>
2024-12-17 18:27:38 +0100

    IT REPO with conditions-training..js

---
+++ PP/conditions-training.js
@@ -0,0 +1,37 @@
+
+// Prvi zadatak
+//Write a program to check if the variable is a number and if it’s a number, check if it is divisible by 2. 
+// If it is, print the result; otherwise, show “X”.
+
+var input = 8; 
+
+if ((typeof input) =='number') {
+
+    if ((input % 2) === 0) {
+        
+        var resultDivision = input / 2;
+
+        console.log(input + " / 2 = " + resultDivision);
+    }
+
+     else {
+
+        console.log ("X");
+    }
+
+}
+
+
+// Drugi Zadatak
+//Write a conditional statement to find the largest of five numbers. Display the result in the console.
+//Sample Numbers: -5, -2, -6, 0, -1
+
+var a = -5
+var b = -2
+var c = -6
+var d = -1 
+
+max = ''
+
+if { }
+
