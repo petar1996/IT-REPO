7cbcfeb0bb0c5489efa88a0ea48596e78788613a
Lenovo <petarstanojkovic@yahoo.ca>
2024-12-23 21:21:42 +0100

    IT REPO third commit loops

--- PP/loops-training.js
+++ PP/loops-training.js
@@ -15,8 +15,11 @@ for (var i = 0; i < 10; i++) {
 */
 
 //2.
-//Write a for loop that will iterate from 0 to 15. For each iteration, check if the current number is odd or even, 
+//Write a for loop that will iterate from 0 to 15. 
+// For each iteration, check if the current number is odd or even, 
 // and display a message.
+
+/*
 
 for (var i = 0; i < 15; i++) {
 
@@ -29,3 +32,51 @@ for (var i = 0; i < 15; i++) {
         console.log(i, " is odd")
     }
 }
+
+*/
+
+// 3.
+//Write a program to sum the multiples of 3 and 5 under 1000.
+
+/*
+
+var sum = 0
+
+for (var i = 0; i < 1000; i++) {
+
+   if ( i % 5 == 0 || i % 3 == 0) {
+
+    sum = sum + i;
+   
+    } else {
+
+        i=i;
+    }
+}
+
+console.log ("Sum is ", sum)
+
+*/
+
+
+
+var sum = 0;
+var product = 1;
+
+var arr = [1, 2, 3, 4, 5];
+
+for ( i = 1 ; i < arr.length ; i++) {
+
+    if ( typeof arr[i] =="number") {
+        
+        sum = sum + arr[i];
+        proudct= product * arr[i];
+        
+    } else {return false}
+
+}
+
+console.log("Sum is :", sum ," and product is ", product)
+
+
+
