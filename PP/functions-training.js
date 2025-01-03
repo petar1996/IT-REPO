e5ac8ca360f139fbeea754cd94d8b03fbdfdae6f
Lenovo <petarstanojkovic@yahoo.ca>
2025-01-03 20:48:03 +0100

    IT REPO n commit functions

--- PP/functions-training.js
+++ PP/functions-training.js
@@ -117,13 +117,14 @@ console.log("Average value is", averageValue(1,2,3,4));
 
 
 
+
+
+
 //8.
 //Count Occurrences in an Array
 //Write a program that calculates the number of appearances of a given number in a given array.
 //Inputs: a = [2, 4, 7, 8, 7, 7, 1], e = 7
 //Result: 3
-
-
 
 function countOccurrences(array) {
 
@@ -143,9 +144,6 @@ function countOccurrences(array) {
 
 }
 
+var a = [1, 1, 2 , 5, 7 , 7 , 8, 7 ] ;
 
-
-
-var array1 = [1, 1, 2 , 5, 7 , 7 , 8, 7 ] ;
-
-console.log("this is the number of ocurrences", countOccurrences(array1));
+console.log("This is the number of ocurrences", countOccurrences(a));
