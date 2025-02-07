452b720f09064e96786a08c444b92d9f2d336a98
Petar Stanojkovic <petar.stanojkovic@centradecheil.onmicrosoft.com>
2025-02-07 16:31:28 +0100

    IT REPO n commit functions

--- PP/functions-training.js
+++ PP/functions-training.js
@@ -1860,3 +1860,11 @@ var final = checkPalindrone ("a nut for a jar of tuna");
 console.log(final);
 
 */
+
+
+
+
+//54. 
+// Find Greatest Common Divisor
+//Write a program that calculates the greatest common divisor of two integers. Note: The greatest common 
+//divisor of two non-zero integers is the greatest positive number that divides both numbers with no remainder.
