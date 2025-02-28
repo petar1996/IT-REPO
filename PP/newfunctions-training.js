b0af58bf0713522ad837689b6767929fcea6ff52
Petar Stanojkovic <petar.stanojkovic@centradecheil.onmicrosoft.com>
2025-02-28 19:56:16 +0100

    IT REPO n commit new functions

--- PP/newfunctions-training.js
+++ PP/newfunctions-training.js
@@ -3,15 +3,15 @@
 //Input: "JavaScript"
 //Output: 3
 
-var countv = function countVowels(a) {
+/*
+
+function countVowels(a) {
 
     var sum = 0;
 
     for ( let i=0 ; i < a.length ; i++) {
 
-        if (a[i] === 'a' || a[i] === 'e' || a[i] === 'i' || a[i] === 'o' || a[i] === 'u' 
-
-            a[i] === 'A' || a[i] === 'E' || a[i] === 'I' || a[i] === 'O' || a[i] === 'U') {
+        if ( a[i] === 'a' || a[i] === 'e' || a[i] === 'i' || a[i] === 'o' || a[i] === 'u' || a[i] === 'A' || a[i] === 'E' || a[i] === 'I' || a[i] === 'O' || a[i] === 'U') {
 
             sum += 1;
 
@@ -25,4 +25,38 @@ var countv = function countVowels(a) {
 
 };
 
-console.log(countv("Javascript"));
+let final = countVowels("JAavascript");
+
+console.log(final);
+
+*/
+
+
+
+
+//Task 2: Combine Two Arrays Alternating Elements
+//Write a function that combines two arrays by alternatingly taking elements.
+//Input: ['a','b','c'], [1,2,3]
+//Output: ['a',1,'b',2,'c',3]
+
+/*
+
+var combine = function combineTwoA (a,b) {
+
+   let newarray = []; 
+
+    for ( let i = 0 ; i < a.length ; i++) {
+
+        newarray[newarray.length] = a[i];
+
+        newarray[newarray.length] = b[i];
+    };
+
+    return newarray;
+
+};
+
+
+console.log (combine( ['a','b','c'], [1,2,3] ));
+
+*/
