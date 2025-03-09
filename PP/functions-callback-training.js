3fc2f8d537af2ae133a2da66c346093f4515a439
Petar Stanojkovic <petar.stanojkovic@centradecheil.onmicrosoft.com>
2025-03-09 17:44:30 +0100

    IT REPO n commit callback

--- PP/functions-callback-training.js
+++ PP/functions-callback-training.js
@@ -1,3 +1,37 @@
 
+//Task 1: Swap First and Last Element in an Array (IIFE)
+//Write an IIFE that replaces the first and last element of the given array 
+//and prints out its elements.
+
+/*
+
+(function(array) { 
+
+    let placeholder = array[array.length -1];
+
+    array[array.length -1] = array[0];
+
+    array[0] = placeholder;
+
+    console.log(array);
+
+}) ([1,2,3,4,5,"ratata"]);
 
 
+*/
+
+
+//Task 2: Calculate Surface Area of a Rectangle (IIFE)
+//Write an IIFE that calculates the surface area of a rectangle with sides a and b.
+
+
+/*
+(function (a,b) {
+
+    let result = a*b;
+
+   console.log(result);
+
+})(3,4);
+
+*/
