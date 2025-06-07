5ec7c2a6bb3b03b3dbd2fb65a2d38439091f836e
Petar Stanojkovic <petar.stanojkovic@centradecheil.onmicrosoft.com>
2025-06-07 15:42:04 +0200

    IT REPO es6

--- PP/BasicObjectExercise/es6-exercise.js
+++ PP/BasicObjectExercise/es6-exercise.js
@@ -3,17 +3,51 @@
 //1. Capitalize Strings
 //Write a function that capitalizes the first letter of each string argument it receives.
 
+/*
 function capitalize(input) {
     return input.filter(item => typeof item === "string").map(item => item.charAt(0).toUpperCase() + item.slice(1));
 }
+
+*/
 
 
 // Task 2
 // Write a function that calculates sales tax using a constant tax rate (e.g. 20%)
 
+/*
+function taxCalc(input) {
+    const tax = 0.20;
 
-function salesTax(price) {
-    const TAX_RATE = 0.20;          // Step 1: define constant tax rate
-    const tax = price * TAX_RATE;   // Step 2: calculate tax
-    return tax;                     // Step 3: return tax amount
-}
+    const fullPrices = input.map(item => {
+        return {
+            name: item.name,
+            price: item.price + (item.price * tax)
+        };
+        });
+
+    const taxOnly = input.map(item => item.price * tax);
+
+    return {
+        fullPrices: fullPrices,
+        taxOnly: taxOnly
+    };
+};
+
+const products = [{ name: "Banana", price: 120 }, { name: "Orange", price: 100 }];
+console.log("Task 2:", taxCalc(products));
+
+*/
+
+
+//3. Increase Array Elements
+//Write a function that increases each element by a given value. If omitted, use 1.
+
+/*
+
+const mapElementIncrease = (array, icrement = 1) => array.map ( item => item + icrement);
+
+console.log ("Task 3:", mapElementIncrease([1,2,4,5,6],));
+
+*/
+
+//4. 
